e3aabc92bb7dd5d6b086c8610211dafbc5b18140
Tsebal <atsebal@gmail.com>
2022-01-18 15:32:56 +0300

    Initial commit

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int year = 3126;
+        System.out.println(year - 8);
+    }
+}
