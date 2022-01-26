d734832c9dba81fea7917429b2a663416b1daddf
Tsebal <atsebal@gmail.com>
2022-01-26 14:53:29 +0300

    Solved

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0302/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task03.task0302;
+
+import java.util.Scanner;
+
+/* 
+Призывная кампания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+        Scanner console = new Scanner(System.in);
+        String name = console.nextLine();
+        int age = console.nextInt();
+        if ((age >= 18) && (age <= 28))
+            System.out.println(name + militaryCommissar);
+    }
+}
