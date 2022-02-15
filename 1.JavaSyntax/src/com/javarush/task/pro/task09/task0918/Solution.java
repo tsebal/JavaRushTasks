962ade072da653d8b840d8c54e9ab650e4e79a90
Tsebal <atsebal@gmail.com>
2022-02-15 19:36:22 +0300

    Solved

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0918/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task09.task0918;
+
+/* 
+Поработаем со StringBuilder
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Учиться, учиться и еще раз учиться! ";
+
+        System.out.println(addTo(string, new String[]{"Под ", "лежачий ", "камень ", "вода ", "не ", "течет"}));
+        System.out.println(replace(string, ", ", 16, 27));
+    }
+
+    public static StringBuilder addTo(String string, String[] strings) {
+        StringBuilder builder = new StringBuilder(string);
+        for (String str : strings) {
+            builder.append(str);
+        }
+        return builder;
+    }
+
+    public static StringBuilder replace(String string, String str, int start, int end) {
+        StringBuilder builder = new StringBuilder(string);
+        return builder.replace(start, end, str);
+    }
+}
