c39332ab0f7b9508113aeb371201d152055f8e4c
Tsebal <atsebal@gmail.com>
2022-02-20 15:34:06 +0300

    Solved

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String text1 = new String();
+        String text2 = new String();
+        String text3 = new String();
+        String text4 = new String();
+        String text5 = new String();
+        String text6 = new String();
+        String text7 = new String();
+        String text8 = new String();
+        String text9 = new String();
+        String text10 = new String();
+        int[] array1 = new int[1];
+        int[] array2 = new int[1];
+        int[] array3 = new int[1];
+        int[] array4 = new int[1];
+        int[] array5 = new int[1];
+        Scanner console = new Scanner(System.in);
+        Scanner buffer = new Scanner(System.in);
+    }
+}
