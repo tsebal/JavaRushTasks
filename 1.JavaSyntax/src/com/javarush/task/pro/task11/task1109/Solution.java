24b6a2993b4f5b1d796b31bb6d8d07c2a78c357c
Dmitriy Aksenenko <dmitrii.asenenko@gmail.com>
2022-02-26 16:18:49 +0300

    Solved

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1109/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task11.task1109;
+
+/* 
+Объекты внутренних и вложенных классов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Outer outerClass = new Outer();
+        Outer.Inner outerInnerClass = outerClass.new Inner();
+        Outer.Nested outerNestedClass = new Outer.Nested();
+    }
+}
