c185efce74f0b10fe60042bd40ca5b956d5be4ab
Tsebal <atsebal@gmail.com>
2022-04-05 15:42:51 +0300

    Solved NIO

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1519/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task15.task1519;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Поверхностное копирование
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Path sourceDirectory = Path.of(scanner.nextLine());
+        Path targetDirectory = Path.of(scanner.nextLine());
+        try (DirectoryStream<Path> sourceDirectories = Files.newDirectoryStream(sourceDirectory)) {
+            for (Path directory : sourceDirectories) {
+                if (Files.isRegularFile(directory)) {
+                    Path newFilePath = targetDirectory.resolve(directory.getFileName());
+                    Files.copy(directory, newFilePath);
+                }
+            }
+        }
+    }
+}
+
