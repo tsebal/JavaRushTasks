8b83170da81fb0ee01db56632c439bfcc0534996
Tsebal <atsebal@gmail.com>
2022-04-08 18:10:27 +0300

    Solved DATE Calendar

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1604/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task16.task1604;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/* 
+День недели рождения твоего
+*/
+
+public class Solution {
+
+    static Calendar birthDate = new GregorianCalendar(1984, 07, 10);
+
+    public static void main(String[] args) {
+        System.out.println(getDayOfWeek(birthDate));
+    }
+
+    static String getDayOfWeek(Calendar calendar) {
+        String dayOfWeek = "";
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (day) {
+            case 1 : return "воскресенье";
+            case 2 : return "понедельник";
+            case 3 : return "вторник";
+            case 4 : return "среда";
+            case 5 : return "четверг";
+            case 6 : return "пятница";
+            case 7 : return "суббота";
+        }
+        return dayOfWeek;
+    }
+}
