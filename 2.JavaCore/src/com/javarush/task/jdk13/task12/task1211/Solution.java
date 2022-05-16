204e0111dbce914b2b8daf55642008362e39506e
Tsebal <atsebal@gmail.com>
2022-05-16 15:41:53 +0400

    Solved abstract, overload methods

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task12/task1211/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.jdk13.task12.task1211;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(5);
+        print(Integer.valueOf(7));
+    }
+
+    public static void print(int a) {
+
+    }
+
+    public static void print(Integer b) {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task12/task1212/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.jdk13.task12.task1212;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print() {
+
+    }
+
+    public static void print(byte a) {
+
+    }
+
+    public static void print(int b) {
+
+    }
+
+    public static void print(long c) {
+
+    }
+
+    public static void print(char d) {
+
+    }
+}
