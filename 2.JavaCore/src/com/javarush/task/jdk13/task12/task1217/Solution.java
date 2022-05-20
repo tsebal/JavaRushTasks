c3e6b176480f90e796f1970b8db7c413e7e62a69
Tsebal <atsebal@gmail.com>
2022-05-20 16:30:58 +0400

    Solved Interfaces

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task12/task1217/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.jdk13.task12.task1217;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+        public void landing();
+    }
+
+}
