5b011a0918b3d90b409b3e695b50b4420a817a77
Tsebal <atsebal@gmail.com>
2022-05-21 18:21:56 +0400

    Solved Interfaces

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task13/task1302/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.jdk13.task13.task1302;
+
+/* 
+Selectable и Updatable
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+
+    public class Screen implements Selectable, Updatable {
+        @Override
+        public void onSelect() {
+
+        }
+
+        @Override
+        public void refresh() {
+
+        }
+    }
+}
