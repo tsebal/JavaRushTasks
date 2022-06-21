648328cb6f88a5c1b055d1757ef600a24e53ef77
”Tsebal” <atsebal@gmail.com>
2022-06-21 17:31:34 +0300

    Solved threads, synchronized, synchronized singleton

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task17/task1701/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.jdk13.task17.task1701;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Заметки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new NoteThread().start();
+        new NoteThread().start();
+    }
+
+    public static class Note {
+
+        public static final List<String> NOTES = new ArrayList<>();
+
+        public static void addNote(String note) {
+            NOTES.add(0, note);
+        }
+
+        public static void removeNote(String threadName) {
+            String note = NOTES.remove(0);
+            if (note == null) {
+                System.out.println("Другая нить удалила нашу заметку");
+            } else if (!note.startsWith(threadName)) {
+                System.out.println("Нить [" + threadName + "] удалила чужую заметку [" + note + "]");
+            } else {
+                System.out.println("Нить [" + threadName + "] удалила свою заметку [" + note + "]");
+            }
+        }
+    }
+
+    public static class NoteThread extends Thread {
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 100; i++) {
+                    Note.addNote(getName() + "-Note" + i);
+                    Thread.sleep(1);
+                    Note.removeNote(getName());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
