dd1cfb93a6614431ece0ceb6519a55ccb06cd883
Tsebal <atsebal@gmail.com>
2022-10-31 17:56:54 +0400

    Solved patterns multithreading: Double check locking

---
+++ 3.JavaMultithreading/src/com/javarush/task/jdk13/task41/task4123/UserDB.java
@@ -0,0 +1,33 @@
+package com.javarush.task.jdk13.task41.task4123;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Objects.isNull;
+
+public class UserDB {
+
+    private volatile Map<Integer, User> users;
+
+    private void initDB() throws Exception {
+
+        if (isNull(users)) {
+            synchronized (this) {
+                if (isNull(users)) {
+                    Map<Integer, User> temp = new HashMap<>();
+                    for (int i = 0; i < 100; i++) {
+                        temp.put(i, new User(i, "user" + i));
+                        TimeUnit.MILLISECONDS.sleep(1);
+                    }
+                    users = temp;
+                }
+            }
+        }
+    }
+
+    public Map<Integer, User> getDB() throws Exception {
+        initDB();
+        return users;
+    }
+}
