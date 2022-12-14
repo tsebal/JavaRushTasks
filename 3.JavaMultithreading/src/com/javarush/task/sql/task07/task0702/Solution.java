d2c5caa6f6075d2af3ab32ca0d93198cb97a7397
Tsebal <atsebal@gmail.com>
2022-12-14 16:54:13 +0400

    Solved JDBC: DriverManager, Connection, Statement

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0702/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.sql.task07.task0702;
+
+import java.sql.*;
+
+/* 
+task0702
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test",
+                "root", "root");
+        Statement statement = connection.createStatement();
+        statement.executeQuery("SELECT 7 * 8");
+        statement.close();
+        connection.close();
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task07/task0703/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.sql.task07.task0703;
+
+import java.sql.*;
+
+/* 
+task0703
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Connection connection  = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/test",
+                "root", "root");
+        Statement statement = connection.createStatement();
+        statement.executeQuery("SELECT * FROM employee");
+        statement.close();
+        connection.close();
+
+    }
+}
