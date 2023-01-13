62b5d417c6aeb68cd2c0035faaf79a20f8bdc5a5
Tsebal <atsebal@gmail.com>
2023-01-13 21:30:07 +0300

    Solved Hibernate collections LazyLoading: Table_linkage: FetchType, @JoinColumn, @LazyCollection

---
+++ 3.JavaMultithreading/src/com/javarush/task/sql/task14/task1402/Publisher.java
@@ -0,0 +1,47 @@
+package com.javarush.task.sql.task14.task1402;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import java.util.List;
+
+@Entity
+@Table(name = "publisher")
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JoinColumn(name = "publisher_id")
+    private List<Book> books;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
